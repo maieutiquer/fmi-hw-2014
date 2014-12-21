b6a1b40286904760cc0a386160c6b5a727d7c50c
daniel rodriguez <maieutiquer@gmail.com>
2014-12-21 20:04:43 +0200

    add solution to hw01 task 1B

---
+++ pointInCircle.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int takeInput(string varName) {
+    int input;
+    cout << "Enter " << varName << ":\n";
+    cin >> input;
+    return input;
+}
+
+double square(double x) {
+    return pow(x, 2);
+}
+
+bool isInCircle(double pointX, double pointY,
+        double centerX, double centerY, double radius) {
+    return square(pointX - centerX) + square(pointY - centerY) <= square(radius);
+}
+
+int main() {
+
+    int pointX;
+    int pointY;
+    int circlesCenterX = 4;
+    int circlesCenterY = -2;
+    double circle1Radius = 4;
+    double circle2Radius = 2;
+    double circle3Radius = 1;
+
+    pointX = takeInput("point X");
+    pointY = takeInput("point Y");
+    bool pointInFigure = (pointX <= 2
+            &&  isInCircle(pointX, pointY, circlesCenterX, circlesCenterY, circle1Radius))
+            || (isInCircle(pointX, pointY, circlesCenterX, circlesCenterY, circle2Radius)
+            && !isInCircle(pointX, pointY, circlesCenterX, circlesCenterY, circle3Radius));
+
+    if (pointInFigure) {
+        cout << "Yes, it's inside." << endl;
+    } else {
+        cout << "No, it's not inside." << endl;
+    }
+
+}
