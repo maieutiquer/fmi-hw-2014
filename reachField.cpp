a3b82a61498ad6e7e609ed312e148c0afff5c8c2
daniel rodriguez <maieutiquer@gmail.com>
2014-12-21 22:25:16 +0200

    improve input code for consistency

--- reachField.cpp
+++ reachField.cpp
@@ -5,28 +5,43 @@ using namespace std;
 
 int takeInput(string varName) {
     int input;
-    do {
-        cout << "Enter " << varName << ":\n";
-        cin >> input;
-
-    } while (input < 0 && input > 7);
+    cout << "Enter " << varName << ":\n";
+    cin >> input;
     return input;
 }
 
-bool checkSameLine(int field1[], int field2[]) {
+void takePosition(int field[], string message) {
+
+    cout << message << endl;
+
+    do {
+        field[0] = takeInput("position x from 1 to 7");
+    } while (field[0] < 0 && field[0] > 7);
+
+    do {
+        field[1] = takeInput("position y from 1 to 7");
+    } while (field[1] < 0 && field[1] > 7);
+
+    cout << endl;
+
+}
+
+bool checkSameLineOrColumn(int field1[], int field2[]) {
     return (field1[0] == field2[0]) || (field1[1] == field2[1]);
 }
 
 int main() {
+
     int field1[2];
     int field2[2];
-    field1[0] = takeInput("field 1 position x from 1 to 7");
-    field1[1] = takeInput("field 1 position y from 1 to 7");
-    field2[0] = takeInput("field 2 position x from 1 to 7");
-    field2[1] = takeInput("field 2 position y from 1 to 7");
-    if (checkSameLine(field1, field2)) {
+
+    takePosition(field1, "Taking field 1 coordinates.");
+    takePosition(field2, "Taking field 2 coordinates.");
+
+    if (checkSameLineOrColumn(field1, field2)) {
         cout << "yes, it can" << endl;
     } else {
         cout << "no, it can't" << endl;
     }
+
 }
